95761bf47bc0a9fc4de7a7c2295f08a32499bca7
Christian Manzaraz <magnusmanz@gmail.com>
2022-12-11 10:49:08 -0300

    Initial Commit

---
+++ AppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Christian Manzaraz on 11/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
